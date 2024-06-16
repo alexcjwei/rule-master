32bff963fb4a58b01ee2817c195a7df64c6f9982
alexcjwei <alexcj.wei@gmail.com>
2024-06-15 17:48:51 -0700

    adds games table

---
+++ prisma/migrations/20240616004835_creates_games_table/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "games" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "games_pkey" PRIMARY KEY ("id")
+);
