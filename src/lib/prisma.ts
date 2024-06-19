f2286d7c149dc53bfa0370c29cd49211970bdf37
alexcjwei <alexcj.wei@gmail.com>
2024-06-19 11:21:10 -0700

    idk see if this helps lol

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,17 +2,11 @@ import { PrismaClient } from '@prisma/client';
 import { PrismaNeon } from '@prisma/adapter-neon';
 import { Pool } from '@neondatabase/serverless';
 
-const getPrisma = () => {
-  const neon = new Pool({ connectionString: process.env.POSTGRES_PRISMA_URL });
-  const adapter = new PrismaNeon(neon);
-  const prisma = new PrismaClient({
-    adapter,
-    log:
-      process.env.NODE_ENV === 'development' ? ['query', 'info', 'warn'] : [],
-  });
-  return prisma;
-};
-
-const prisma = getPrisma();
+const neon = new Pool({ connectionString: process.env.POSTGRES_PRISMA_URL });
+const adapter = new PrismaNeon(neon);
+const prisma = new PrismaClient({
+  adapter,
+  log: process.env.NODE_ENV === 'development' ? ['query', 'info', 'warn'] : [],
+});
 
 export default prisma;
