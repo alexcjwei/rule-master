b614fac3d81b2ed451acc7b867e631ff5f941f3d
alexcjwei <alexcj.wei@gmail.com>
2024-06-19 12:46:17 -0700

    removes middleware for now

--- src/app/api/files/route.ts
+++ src/app/api/files/route.ts
@@ -1,11 +1,8 @@
 import prisma from '@/lib/prisma';
 import { s3Client, S3_BUCKET_NAME, formatS3Key } from '@/lib/s3';
 import { Upload } from '@aws-sdk/lib-storage';
-import { middleware } from '@/middleware';
 
 export async function POST(request: Request) {
-  const session = await middleware();
-  const user = session?.user;
   const formData = await request.formData();
   const file = formData.get('file') as File;
   const gameId = Number(formData.get('gameId'));
@@ -21,7 +18,6 @@ export async function POST(request: Request) {
         data: {
           name: file.name,
           game: { connect: { id: gameId } },
-          createdBy: { connect: { id: user?.id } },
         },
       });
 
