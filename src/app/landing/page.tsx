7ffeef734aad7555d029debcf1aad9e761122cef
alexcjwei <alexcj.wei@gmail.com>
2024-06-21 12:42:35 -0700

    adds some text to landing page

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,3 +1,14 @@
 export default function Landing() {
-  return <div>Hello, Landing Page</div>;
+  return (
+    <div>
+      <h1 className='text-xl'>Welcome to RuleMaster</h1>
+      <p>
+        RuleMaster is your ultimate destination for board game rules and Q&A.
+      </p>
+      <p>
+        Explore our collection of board games and their rulebooks, and get your
+        questions answered by our Rule Answering Guru (RAG).
+      </p>
+    </div>
+  );
 }
